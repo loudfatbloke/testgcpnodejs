3adb22f1937d9fd0933c0286b59ab5549ec43e1a
loudfatbloke <93079198+loudfatbloke@users.noreply.github.com>
2022-03-19 13:50:31 +0000

    Add files via upload
    new test

---
+++ web.js
@@ -0,0 +1,21 @@
+//web hello world that echos url parameters
+var http = require('http');
+var url = require('url');
+
+var util = require('util') ; 
+ var args = process.argv.slice(2);
+var xx = util.format(" %s %s  ",  "a" , "B" );
+  console.log(xx);
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write('<h1> Hello World! </h1> \n');
+  
+ res.write(args[0]);
+
+  var q = url.parse(req.url, true).query;
+  var txt = q.who + " " + q.loves;
+ res.write(txt);
+res.end("</body> </html>");
+}).listen(8081);
