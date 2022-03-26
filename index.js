c4506802dd63649ec84e607b3bac07dcc1e34bf0
loudfatbloke <93079198+loudfatbloke@users.noreply.github.com>
2022-03-26 13:04:53 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//web hello world that echos url parameters
+const http = require('http');
+var url = require('url');
+
+
+const express = require('express')
+var app = express()
+ 
+app.get('/', (req, res) => {
+ res.send('Hello World!  <p> pants <p> pants <p> pants')
+ 
+  })
+ 
+
+app.listen(3001, () => {
+  console.log(`Example app listening on port 3000!`)
+})
+module.exports = app;
