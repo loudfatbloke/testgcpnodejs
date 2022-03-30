8e7149f139e4f17fee0f5a3375a0c502553443ae
loudfatbloke <93079198+loudfatbloke@users.noreply.github.com>
2022-03-30 15:36:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 //web hello world that echos url parameters
+//
 const http = require('http');
 var url = require('url');
+require('dotenv').config();
+// :wqrequire('Math');
 
 
 const express = require('express')
 var app = express()
- 
+var xxx = 'Hello World!  <p> pants <p> pants <p> pants <p> <b>' + Math.random();
+
+        + process.env.TESTIT;
 app.get('/', (req, res) => {
- res.send('Hello World!  <p> pants <p> pants <p> pants')
- 
+ res.send(xxx)
+
   })
- 
+
 
 app.listen(8080, () => {
   console.log(`Example app listening on port 3000!`)
+  console.log(xxx)
 })
 module.exports = app;
+
